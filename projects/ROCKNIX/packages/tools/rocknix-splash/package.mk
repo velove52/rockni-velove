86f00a96831c2b3e98531404ed8edb5cb6627681
velove <50912588+velove52@users.noreply.github.com>
2026-06-07 23:02:31 +0800

    Update package.mk

--- projects/ROCKNIX/packages/tools/rocknix-splash/package.mk
+++ projects/ROCKNIX/packages/tools/rocknix-splash/package.mk
@@ -2,9 +2,9 @@
 # Copyright (C) 2025 ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="rocknix-splash"
-PKG_VERSION="9d295bcb74be2282e32c6b614efbea6036974ba8"
+PKG_VERSION="3f50ab02052b6c0077ab8f8fcea655247c42d857"
 PKG_LICENSE="GPL"
 PKG_SITE="https://rocknix.org"
-PKG_URL="https://github.com/ROCKNIX/${PKG_NAME}/archive/${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/velove52/${PKG_NAME}/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_INIT="toolchain"
-PKG_LONGDESC="ROCKNIX splash screen application"
+PKG_LONGDESC="VELOVE splash screen application"
